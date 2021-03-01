b5756eca82dfc618234a06f0569e59954e50ad6f
Ahmad Dahshan <dahshana95@gmail.com>
2021-03-01 13:30:41 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,35 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Post Data into Pardot</title>
 </head>
-<body>
-    <h1>Post Data into Pardot Using PHP</h1>
-    <form action="post-to-pardot.php" method="post">
-        <div>
-            <label for="txtFirstName">First Name:</span>
-            <input type="text" name="txtFirstName" id="txtFirstName" required>
-        </div>
+    <body>
+        <h1>Post Data into Pardot Using PHP</h1>
+        <form action="post-to-pardot.php" method="post">
+            <div>
+                <label for="txtFirstName">First Name</label>
+                <input type="text" name="txtFirstName" id="txtFirstName" required>
+            </div>
 
-        <div>
-            <label for="txtLastName">Last Name:</span>
-            <input type="text" name="txtLastName" id="txtLastName" required>
-        </div>
+            <div>
+                <label for="txtLastName">Last Name</label>
+                <input type="text" name="txtLastName" id="txtLastName" required>
+            </div>
 
-        <div>
-            <label for="txtEmail">Email:</span>
-            <input type="email" name="txtEmail" id="txtEmail" required>
-        </div>
+            <div>
+                <label for="txtEmail">Email:</label>
+                <input type="email" name="txtEmail" id="txtEmail" required>
+            </div>
 
-        <div>
-            <label for="txtPhone">Phone:</span>
-            <input type="text" name="txtPhone" id="txtPhone" required>
-        </div>
+            <div>
+                <label for="txtPhone">Phone:</label>
+                <input type="text" name="txtPhone" id="txtPhone" required>
+            </div>
 
-        <div>
-            <input type="submit" name="submit" value="Send">
-        </div>
-    </form>
-</body>
+            <div>
+                <input type="submit" name="submit" value="Send">
+            </div>
+        </form>
+    </body>
 </html>
